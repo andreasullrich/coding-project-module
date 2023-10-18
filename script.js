a6ad0f28119be48abbbda94bb179121e8667b091
Andreas Ullrich <ullrich.andi@web.de>
2023-10-18 22:00:28 +0200

    task: spot problems and solve them

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
-//print a Hello World message to the console of the browsers devtools
-console.log("Hello World");
+//Use the concatenation operator to concatenate each letter of your firstname into a string one letter by one.
+console.log("A" + "n" + "d" + "r" + "e" + "a" + "s");
+
+// Error! Make it work console.log("Hallo";
+console.log("Hallo");
+
+// Error! Make it work console.log("That doesn"t work");
+console.log('That doesn"t work');
+
+// should return true. Change only the operator to fix it. console.log(50 + 2 > 52);
+console.log(50 + 2 >= 52);
+
+// should be false. Change only the operator to fix it. console.log("333" == 333);
+console.log("333" === 333);
+
+// Wrong result: Expected 555 not 855. Change only the operator to fix it. console.log(600 + 510 / 2);
+console.log((600 + 510) / 2);
+
+// Wrong result: Expected a Number 4 received 22 console.log("2" + 2);
+console.log(2 + 2);
+
+// Create a console.log that returns true when both division results are equal to 5
+console.log(25 / 5 == 5);
+console.log(50 / 10 == 5);
