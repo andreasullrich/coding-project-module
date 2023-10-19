cc825c9bdb6929315ff07cb87495f9f06927a8f1
Andreas Ullrich <ullrich.andi@web.de>
2023-10-19 11:55:55 +0200

    task: addPositive function update to one formel/function

--- functions/script.js
+++ functions/script.js
@@ -13,17 +13,9 @@ console.log(calcGrossPrice(20, 0.19));
 console.log(calcGrossPrice(40, 0.16));
 
 //Implement a addPositive function.
-function addPositive(number1, number2) {
-  return number1 + number2;
+function addPositive(nmbr1, nmbr2) {
+  return Math.abs(nmbr1) + Math.abs(nmbr2);
 }
 console.log(addPositive(2, 3));
-
-function addPositive(number1, number2) {
-  return number1 - number2;
-}
 console.log(addPositive(3, -5));
-
-function addPositive(number1, number2) {
-  return (number1 + number2) * -1;
-}
 console.log(addPositive(-1, -8));
